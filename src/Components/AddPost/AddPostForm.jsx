57d062e2ba50709eff240e0ae33d7f0778b3cd8e
Andre <andrebennett106@gmail.com>
2022-04-25 08:21:25 -0400

    Updated contents of AddPostForm file

---
+++ src/Components/AddPost/AddPostForm.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+
+const AddPostForm = (props) => {
+    
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newName = {
+            name: name,
+        };
+        console.log(newName);
+        props.addNewName(newName)
+        let newPost = {
+            post: post,
+        };
+        console.log(newPost);
+        props.addNewPost(newPost)
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit} action='' method='post'>
+            <div className='form-group'>
+                <label for='Name'>Name:</label>
+                <input type='name' className='form-control' id='exampleInputName' placeholder='Enter Name' value={name} onChange={(event) => setName(event.target.value) }/>
+            </div>
+            <div className='form-group'>
+                <label for='Post'>Post:</label>
+                <input type='post' className='form-control' id='exampleInputPost' placeholder='Enter Post' value={post} onChange={(event) => setPost(event.target.value) }/>   
+            </div>
+           <button type = 'submit'>Create</button>
+        </form>
+    
+     
+     );
+}
+ 
+export default AddPostForm;
