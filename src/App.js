e0a4c22705f903b7292169a3e3af7ee94369cec3
Andre <andrebennett106@gmail.com>
2022-04-25 08:20:13 -0400

    Setup of project all files and folders created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState,  } from 'react';
+import DisplayPosts from './Components/DisplayPosts/DisplayPosts'
+import AddPostForm from './Components/AddPost/AddPostForm'
+import DisplayNames from './Components/DisplayNames/DisplayNames';
 import './App.css';
 
+
+
 function App() {
+
+const [names, setName] = useState([{name:'Andre'}])
+const [posts, setPost] = useState([{post: 'Hi'}])
+
+function addNewName(name){
+  
+  let tempName = [...names, name];
+  setName(tempName);
+
+function addNewPost(post){
+  
+  let tempPost = [...posts, post];
+  setPost(tempPost);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayNames parentName={names} />
+          </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayPosts parentPost={posts} />
+          </div>
+          <button type='submit' className='btn btn-primary'>Like</button>
+          <button type='submit' className='btn btn-primary'>Dislike</button>
+          <div className='col-md-6'>
+            <div className='border-box'>
+              <AddPostForm addNewPost={addNewPost} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    </div>
+    
+
   );
 }
-
+ }
 export default App;
